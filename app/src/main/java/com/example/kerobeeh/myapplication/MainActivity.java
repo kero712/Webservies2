1dfe3162c157a0aa676e060c1e388cb4fc8a0d31
sign3 <kokogerges02@gmail.com>
2017-10-02 20:58:01 +0200

    new

--- app/src/main/java/com/example/kerobeeh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kerobeeh/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.kerobeeh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,19 +34,66 @@ public class MainActivity extends AppCompatActivity {
     Datamodelmovie datamodelmovie[];
     MovieArrayAdapter movieArrayAdapter;
     ListView listView;
-
+    String sort_By_Varible;
+   // RadioGroup btnsearch;
+    final String ReleaseDatedesc = "release_date.desc";
+    final String Popularitydesc = "popularity.desc";
+    final String ReleaseDateAsc = "release_date.asc";
+    final String PopularityAsc = "popularity.asc";
+     RadioGroup radioGroup;
+    RadioButton btn1 ;
+    RadioButton btn2 ;
+    RadioButton btn3 ;
+    RadioButton btn4 ;
+    Button search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView=(ListView)findViewById(R.id.listmovies);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+         btn1 = (RadioButton) findViewById(R.id.rad1);
+         btn2 = (RadioButton) findViewById(R.id.rad2);
+         btn3 = (RadioButton) findViewById(R.id.rad3);
+         btn4 = (RadioButton) findViewById(R.id.rad4);
+         search  = (Button) findViewById(R.id.btnsearch);
+        search.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (radioGroup.getCheckedRadioButtonId()) {
+                    case R.id.rad1:
+                        sort_By_Varible = "popularity.desc";
+                        break;
+                    case R.id.rad2:
+                        sort_By_Varible = "release_date.desc";
+                        break;
+                    case R.id.rad3:
+                        sort_By_Varible = "release_date.asc";
+                        break;
+                    case R.id.rad4:
+                        sort_By_Varible = "popularity.asc";
+                        break;
+                }
+                String url = "http://api.themoviedb.org/3/discover/movie?api_key=ab71bd22b9e87623e779800766e9ef81&sort_by="+ sort_By_Varible ;
+
+                executeWebService(url);
+
+            }
+
+
+
+        });
+    }
+
+    private void executeWebService(String url) {
 
        // final TextView mTextView = (TextView) findViewById(R.id.text);
        final ImageView mImageView =(ImageView)findViewById(R.id.imageView);
        final Gson gson = new Gson();
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://api.themoviedb.org/3/discover/movie?api_key=882f561be016ad898b8e88fa110a9536";
+        String s = "http://api.themoviedb.org/3/discover/movie?api_key=882f561be016ad898b8e88fa110a9536";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -69,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), " rferer ", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
 
                 //  mTextView.setText("That didn't work!");
             }
